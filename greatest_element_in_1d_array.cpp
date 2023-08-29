71b8e5efe900b374a88090a1caebfc1576da7f10
jatin_893_joshi <jhjoshi300504@gmail.com>
2023-08-30 01:55:30 +0530

    Greatest Element in one dimensional Array

---
+++ greatest_element_in_1d_array.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int greatest_element(int array[],int size)
+{
+    int greatest=0;
+    for(int i=0;i<size;i++)
+    {
+        if(greatest<array[i])
+        {
+            greatest=array[i];
+        }
+    }
+
+    return greatest;
+}
+int main()
+{
+    int n;
+    cout<<"Enter the sixe of the array: \t";
+    cin>>n;
+    int array[n];
+    cout<<"Enter the elements in the array:\n";
+    for(int i=0;i<n;i++)
+    {
+        cin>>array[i];
+    }
+    cout<<"The greatest element is "<<greatest_element(array,n);
+    return 0;
+
+}
