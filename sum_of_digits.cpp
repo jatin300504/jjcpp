04bea89fd82608db42aa3f8d4c32dcebea8ac737
jatin_893_joshi <jhjoshi300504@gmail.com>
2023-08-31 00:49:23 +0530

    Checking Palindrom (In numbers)

---
+++ sum_of_digits.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+unsigned short sum_of_digits(long long int n)
+{
+    unsigned short sod=0; //sod stands for 'sum of digits'
+    while (n>0)
+    {
+        sod+=n%10;
+        n/=10;
+    }
+    return sod;
+}
+int main()
+{
+    long long int n;
+    cout<<"Enter the value :\t";
+    cin>>n;
+    cout<<"The sum of the digits of  "<<n<<"  is "<<sum_of_digits(n);
+    return 9;
+    }
