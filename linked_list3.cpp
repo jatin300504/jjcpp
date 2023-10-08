17301c7cd86b1cfc6a6b6dc8df9e58694f5da1cf
Jatin <jhjoshi300504@gmail.com>
2023-10-08 13:24:03 +0530

    ttyl

--- linked_list3.cpp
+++ linked_list3.cpp
@@ -1,26 +1,14 @@
 #include<bits/stdc++.h>
+#include<algorithm>
+#include<string>
 using namespace std;
 class node{
-    public:
-    int data;
-    node* next;
-    
+public:
+int info;
+ node* next;
 };
-node* insert_in_linked_list(node *& start,int value)
-{
-    node* new_node=new node;
-    new_node->next=NULL;
-    new_node->data=value;
-    start=new_node;
-    return new_node;
-}
 int main()
 {
-    node*start=NULL;
-    node* new_node=new node;
-    int value;
-    cout<<"Enter the data for the second node: "<<endl;
-    cin>>value;
-    new_node=insert_in_linked_list(start,value);
     
+return 0;
 }
