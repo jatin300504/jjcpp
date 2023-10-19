facd91947970a3b1345d1acdd6aa95193987a4e0
Jatin <Jhjoshi300504@gmail.com>
2023-10-19 16:57:39 +0530

    Quick Sort

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -41,16 +41,25 @@ int partition(int arr[],int low,int high)
 }
 int main()
 {
-    int arr[]={1,3,5,6,7,8,9,2,1,3,4,5};
-    cout<<"Before\t";
-    for(int i=0;i<12;i++)
+    int n;
+    cout<<"Enter the number of elements\t";
+    cin>>n;
+    int* arr=new int[n];
+    cout<<"Enter the elements in the array"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    cout<<"Before Sorting\t";
+    for(int i=0;i<n;i++)
     {
         cout<<" "<<arr[i];
     }
     cout<<endl;
-    quick_sort(arr,0,11);
-    cout<<"after sorting\t";
-    for(int i=0;i<12;i++)
+    quick_sort(arr,0,n-1);
+    cout<<"After sorting\t";
+    for(int i=0;i<n;i++)
     {
         cout<<" "<<arr[i];
     }
